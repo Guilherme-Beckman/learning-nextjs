8b69609c74fcde50f3d2da3134c531ca8854be54
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-07-24 16:58:30 -0400

    feat: add metadata for Contatos page

--- my-app/src/app/contatos/page.tsx
+++ my-app/src/app/contatos/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata ={
+    title: "Contatos",
+    description: "Pagina de contatos"
+}
 export default function Contatos(){
     return(
         <div>
